d8430356099676ce990aa1ac7a28f105c2fc3330
LeviTas2003 <levitas2003@gmail.com>
2023-01-09 10:50:40 +0100

    Initial commit

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+    // Database constanten
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'mvc-2109b');
+
+    // Het pad naar de app map
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    // De URL van de site
+    define('URLROOT', 'http://www.mvc-2109b.org');
+    
+    // Naam van de site
+    define('SITENAME', 'MVC-2109B');
+?>
