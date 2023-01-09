d7951927f20504de7cc0d8b8aa58943460806659
LeviTas2003 <levitas2003@gmail.com>
2023-01-09 11:12:21 +0100

    Made database

--- app/config/config.php
+++ app/config/config.php
@@ -1,15 +1,15 @@
 <?php
-    // Database constanten
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-    define('DB_NAME', 'mvc-2109b');
+// Database constanten
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'mvc-framework');
 
-    // Het pad naar de app map
-    define('APPROOT', dirname(dirname(__FILE__)));
+// Het pad naar de app map
+define('APPROOT', dirname(dirname(__FILE__)));
 
-    // De URL van de site
-    define('URLROOT', 'http://mvc-oop-pdo-toets-userstory.com');
-    
-    // Naam van de site
-    define('SITENAME', 'MVC-2109B');
+// De URL van de site
+define('URLROOT', 'http://mvc-oop-pdo-toets-userstory.com');
+
+// Naam van de site
+define('SITENAME', 'MVC-2109B');
